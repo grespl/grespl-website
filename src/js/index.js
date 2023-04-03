79498a283c3dc64b63ea122f5a4fa37114897516
SwapnilGrespl <120772063+SwapnilGrespl@users.noreply.github.com>
2023-04-03 16:08:58 +0530

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
 // This is Javascript File.
+console.log('working done');
+
+function changeBg(){
+   var navbar = document.getElementById('menuWrapper');
+   var scrollValue = window.scrollY;
+   if(scrollValue < 150){
+    navbar.classList.remove('bgColor');
+   }else{
+    navbar.classList.add('bgColor');
+   }
+}
+window.addEventListener('scroll',changeBg);
+
+
+
+
+
+
+// footer accordion section 
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const accordionContent = header.nextElementSibling;
+    
+    if (accordionContent.style.display === 'block') {
+      accordionContent.style.display = 'none';
+    } else {
+      accordionContent.style.display = 'block';
+    }
+  });
+});
+
+
+
+// sidebar section in mobile view
+$(document).ready(function(){
+    $('#menu-primary').children().click(function(){
+        $(this).children('.sub-menu').toggle();     
+    });
+});
