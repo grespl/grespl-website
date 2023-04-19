0fa7cc6a54b11f407b7e0a3b694e48017c114ffb
SwapnilGrespl <120772063+SwapnilGrespl@users.noreply.github.com>
2023-04-19 17:12:35 +0530

    Updated Accordion section in footer

--- src/js/index.js
+++ src/js/index.js
@@ -15,30 +15,65 @@ window.addEventListener('scroll',changeBg);
 
 
 
-
+                                                                                                                                                                                                                                                                                                                                              
 
 // footer accordion section 
-const accordionHeaders = document.querySelectorAll('.accodion__icon');
 
-accordionHeaders.forEach(header => {
-  header.addEventListener('click', () => {
-    const accordionItem = header.parentElement;
-    const accordionContent = accordionItem.querySelector('.accordion-body');
-    const isActive = accordionItem.classList.contains('active');
 
-    accordionHeaders.forEach(header => {
-      header.parentElement.classList.remove('active');
-    });
-
-    if (!isActive) {
-      accordionItem.classList.add('active');
-      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-    } else {
-      accordionItem.classList.remove('active');
-      accordionContent.style.maxHeight = null;
-     }
+function toggleCRC (thisChevron,thisID,thisHead) {
+  if($(thisChevron).hasClass("on")) {
+      $(thisChevron).removeClass("on");
+      $(thisChevron).css("transform","rotate(0deg)");
+  } else {
+    
+    $(thisChevron).addClass("on");
+      $(thisChevron).css("transform","rotate(90deg)");
+  }
+  $("#"+thisID).slideToggle( 600, "swing", function() {
   });
+}
+
+/* open accordian */
+$(".acc-head").click(function() {
+  
+  var expandThisID = $(this).data("section");
+  var thisHead = $('.acc-head[data-section=' + expandThisID +']');
+  var chevron = $('i[data-section=' + expandThisID +']');
+  // close open chevrons
+  $(".crc-chevron").each (function() {
+    if ($(this).hasClass("on")) {
+    }
+  })
+  // close open accordions
+  
+   $(".acc-body").each(function() {
+     if ($(this).data("section") != expandThisID && $(this).is(":visible")) {
+        $("#"+$(this).data("section")).slideToggle( 600, "swing", function() {
+        });
+        $("i[data-section=" + $(this).data("section") + ']').removeClass("on");
+        $("i[data-section=" + $(this).data("section") + ']').css("transform","rotate(0deg)");
+      }
+    });
+  
+  $("#"+expandThisID+" button").removeClass("crc-close-anim");
+  
+  toggleCRC(chevron,expandThisID,thisHead);   
 });
+
+/* X functionality */
+$('.close').click(function() { // fa-times class
+  $(this).addClass("crc-close-anim");
+  var collapseThisID = $(this).data("section");
+  //console.log(collapseThisID);
+  var chevron = $('i[data-section=' + collapseThisID +']');
+  var thisHead = $('.acc-head[data-section=' + collapseThisID +']');
+  toggleCRC(chevron,collapseThisID,thisHead)	
+});
+
+
+
+
+
 
 
 // sidebar section in mobile view
